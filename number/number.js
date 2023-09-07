a3ea3486e67a8cf54c74049f9efd7490d7d39e16
Thomas Nguyen <ntthptgv@gmail.com>
2023-09-07 18:33:13 +0700

    source code video 6

---
+++ number/number.js
@@ -0,0 +1,8 @@
+console.log('Hello world from nt'.toUpperCase())
+let a = 5;
+let b = 6;
+let c = 7.9;
+let d = '5';
+let e = +d;
+console.log('e value: ', e, 'type: ', typeof e)
+console.log(a === e, 'a: ', typeof a, 'd: ', typeof d)
