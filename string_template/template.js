5277920ea687d705aca81878b3142036d9b4d7d5
Thomas Nguyen <ntthptgv@gmail.com>
2023-09-07 19:04:47 +0700

    source code video 7

---
+++ string_template/template.js
@@ -0,0 +1,7 @@
+console.log('Hello world from NT')
+let a = 5;
+let b = 10;
+console.log('Fifteen is ' + (a + b) + ' and\nnot ' + (2 * a + b) + '.');
+console.log('Fifteen is ' + a + b + ' and not ' + (2 * a + b) + '.');
+
+console.log(`Gia tri a = ${a} b = ${b} a+b = ${a + b}`)
