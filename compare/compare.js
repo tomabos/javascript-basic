0f2decbaf033f7878b470487e85c04e026624bfe
Thomas Nguyen <ntthptgv@gmail.com>
2023-09-09 22:30:15 +0700

    source code video 11

---
+++ compare/compare.js
@@ -0,0 +1,6 @@
+console.log('Hello world')
+let a = 5;
+let b = '5';
+
+console.log('compare a==b: ', 'a = ', a, ' b = ', b, ' result: ', a == b);
+console.log('compare a===b: ', 'a = ', a, ' b = ', b, ' result: ', a === b);
